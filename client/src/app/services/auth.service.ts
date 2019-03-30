c835661b70c22b831e422b3678be459d0e43ecf6
sanket293 <scvagadiya@gmail.com>
2019-03-30 17:10:10 -0400

    updated heroku link

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   user: User;
   private authToken: any;
 
-   private endpoint = 'https://sanketkumar.herokuapp.com/api';
+   private endpoint = 'https://sanketkumar.herokuapp.com/api/';
   // private endpoint = 'http://localhost:3000/api/';
 
   private httpOptions = {
